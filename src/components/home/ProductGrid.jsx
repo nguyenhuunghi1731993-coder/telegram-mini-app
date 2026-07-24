92c6cade031d5a0826c32d4269e97fde96b4eb00
nghi nguyen <huunghind888@gmail.com>
2026-07-25 00:55:40 +0700

    Update mobile product grid

--- src/components/home/ProductGrid.jsx
+++ src/components/home/ProductGrid.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 function ProductGrid({ products }) {
   return (
-    <div className="mx-auto grid w-full grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-3 gap-2">
       {products.map((product) => (
         <ProductCard
           key={product.id}
